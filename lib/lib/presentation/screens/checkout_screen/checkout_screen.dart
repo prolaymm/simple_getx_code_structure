d2e9345c8221386c664eae11431bed1c9808f91d
prolaymm <prolaymm@gmail.com>
2022-07-11 16:24:04 +0630

    FEAT : checkout screen's address detail product list added

--- lib/lib/presentation/screens/checkout_screen/checkout_screen.dart
+++ lib/lib/presentation/screens/checkout_screen/checkout_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_core/get_core.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:mya_ngwe_san/lib/presentation/reusable_widget/custom_button.dart';
 import 'package:mya_ngwe_san/lib/presentation/reusable_widget/text_view.dart';
 import 'package:mya_ngwe_san/lib/presentation/router_hadling/route_page_name.dart';
 import 'package:mya_ngwe_san/lib/presentation/screens/checkout_screen/deliver_address_info.dart';
@@ -10,6 +11,7 @@ import 'package:mya_ngwe_san/lib/utils/fonts_and_margins_constants.dart';
 import '../../reusable_widget/app_bar_with_drawer.dart';
 import '../../reusable_widget/custom_product_container.dart';
 import 'checkout_product.dart';
+import 'order_summary.dart';
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({Key? key}) : super(key: key);
@@ -21,95 +23,132 @@ class CheckoutScreen extends StatelessWidget {
         title: 'Check Out',
         isBackArrow: true,
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: kDefaultMarginWidth.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextView(title: "Delivery Information", fontSize: kLargeFont16.sp),
-            SizedBox(
-              height: kDefaultMarginHeight.h,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextView(
-                  title: "Address",
-                  fontSize: kMediumFont14.sp,
-                  color: Theme.of(context).hintColor,
-                ),
-                GestureDetector(
-                    onTap: () => Get.toNamed(RoutePageName.rMyAddress),
-                    child: TextView(
-                      title: "Change",
-                      fontSize: kMediumFont14.sp,
-                      color: Theme.of(context).primaryColor,
-                    )),
-              ],
-            ),
-            SizedBox(
-              height: kDefaultMarginHeight.h,
-            ),
-            const DeliveryAddress(),
-            SizedBox(
-              height: kDefaultMarginHeight*2.h,
-            ),
-            TextView(title: "Delivery Information", fontSize: kLargeFont16.sp),
-            SizedBox(
-              height: kDefaultMarginHeight.h,
-            ),
-            OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                side: BorderSide(width: 1.0, color: Theme.of(context).primaryColor),
-             //   foregroundColor: MaterialStateProperty.all(Theme.of(context).primaryColor),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: kDefaultMarginWidth.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextView(
+                  title: "Delivery Information", fontSize: kLargeFont16.sp),
+              SizedBox(
+                height: kDefaultMarginHeight.h,
               ),
-                onPressed: () => debugPrint("Comming Soon"),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 14.h),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      TextView(
-                        title: "Cash On Delivery",
-                        fontSize: kLargeFont16.sp,
-                        maxLine: 3,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      SizedBox(
-                        width: kDefaultMarginWidth.w,
-                      ),
-                      Icon(
-                        Icons.check_circle,
-                        size: 22.sp,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextView(
+                    title: "Address",
+                    fontSize: kMediumFont14.sp,
+                    color: Theme.of(context).hintColor,
                   ),
-                )),
-            SizedBox(
-              height: kDefaultMarginHeight.h,
-            ),
-            const Divider(),
-            SizedBox(
-              height: kDefaultMarginHeight.h,
-            ),
-            SizedBox(
-              height: 112.h+112.w,
-
-              child: ListView.builder(
-                  itemCount: 20,
-               //   scrollDirection: Axis.horizontal,
+                  GestureDetector(
+                      onTap: () => Get.toNamed(RoutePageName.rMyAddress),
+                      child: TextView(
+                        title: "Change",
+                        fontSize: kMediumFont14.sp,
+                        color: Theme.of(context).primaryColor,
+                      )),
+                ],
+              ),
+              SizedBox(
+                height: kDefaultMarginHeight.h,
+              ),
+              const DeliveryAddress(),
+              SizedBox(
+                height: kDefaultMarginHeight * 2.h,
+              ),
+              TextView(
+                  title: "Delivery Information", fontSize: kLargeFont16.sp),
+              SizedBox(
+                height: kDefaultMarginHeight.h,
+              ),
+              OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(
+                        width: 1.0, color: Theme.of(context).primaryColor),
+                    //   foregroundColor: MaterialStateProperty.all(Theme.of(context).primaryColor),
+                  ),
+                  onPressed: () => debugPrint("Comming Soon"),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 14.h),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextView(
+                          title: "Cash On Delivery",
+                          fontSize: kLargeFont16.sp,
+                          maxLine: 3,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        SizedBox(
+                          width: kDefaultMarginWidth.w,
+                        ),
+                        Icon(
+                          Icons.check_circle,
+                          size: 22.sp,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ],
+                    ),
+                  )),
+              SizedBox(
+                height: kDefaultMarginHeight.h,
+              ),
+              const Divider(),
+              SizedBox(
+                height: kDefaultMarginHeight.h,
+              ),
+              ListView.builder(
+                  itemCount: 2,
+                  physics: const NeverScrollableScrollPhysics(),
+                  //   scrollDirection: Axis.horizontal,
                   shrinkWrap: true,
                   itemBuilder: (co_, position) {
                     return Container(
-                      margin: EdgeInsets.symmetric(
-                          horizontal: kSmallMargin.w,vertical: kSmallMargin.h),
-                      height: 70.h+70.w,
-                      width:double.infinity,
-                      child: CheckoutProduct(title: 'Dell Inspiron dadad ada dad ada', imageUrl: 'https://cdn.pixabay.com/photo/2016/03/23/04/44/fruits-and-vegetables-1274079_960_720.png', price: 'MMK 20000', subtitle: 'M6R4', qty: '20',)
-                    );
+                        margin: EdgeInsets.symmetric(
+                            horizontal: kSmallMargin.w,
+                            vertical: kSmallMargin.h),
+                        height: 70.h + 70.w,
+                        width: double.infinity,
+                        child: const CheckoutProduct(
+                          title: 'Dell Inspiron dadad ada dad ada',
+                          imageUrl:
+                              'https://cdn.pixabay.com/photo/2016/03/23/04/44/fruits-and-vegetables-1274079_960_720.png',
+                          price: 'MMK 20000',
+                          subtitle: 'M6R4',
+                          qty: '20',
+                        ));
                   }),
-            ),
+              const OrderSummary(),
+              SizedBox(
+                height: 20.h,
+              )
+            ],
+          ),
+        ),
+      ),
+
+      bottomNavigationBar: Container(
+        height: 80.h,
+        padding: EdgeInsets.symmetric(horizontal: kDefaultMarginWidth.w,vertical: kDefaultMarginHeight.h),
+        color: Theme.of(context).primaryColor,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+
+            RichText(
+
+                text: TextSpan(
+              children: [
+                TextSpan(text:"Total\n",style: TextStyle(color: Theme.of(context).colorScheme.primaryContainer,fontSize: kSmallFont12.sp) ),
+                TextSpan(text:"25000000 MMK",style: TextStyle(color: Colors.yellow,fontSize: kMediumFont14.sp) ),
+              ]
+            )),
+
+            
+            CustomButton(buttonText: "Pay Now", onClick: ()=> debugPrint("hello world"),radius: 8.r,buttonColor: Theme.of(context).colorScheme.primaryContainer,)
+
           ],
         ),
       ),
--- lib/lib/presentation/screens/checkout_screen/order_summary.dart
+++ lib/lib/presentation/screens/checkout_screen/order_summary.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mya_ngwe_san/lib/presentation/reusable_widget/custom_dash_widget.dart';
+import 'package:mya_ngwe_san/lib/presentation/reusable_widget/text_view.dart';
 import 'package:mya_ngwe_san/lib/utils/fonts_and_margins_constants.dart';
 
 class OrderSummary extends StatelessWidget {
@@ -15,6 +17,39 @@ class OrderSummary extends StatelessWidget {
           horizontal: kDefaultMarginWidth.w, vertical: kDefaultMarginHeight.h),
 
 
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextView(title: "Order Summary", fontSize: kExtraLargeFont18.sp,color: Theme.of(context).primaryColor,),
+          SizedBox(
+            height: kLargeMarginHeight.h,
+          ),
+           CustomDashWidget(widget: const SizedBox(height: 1,width: double.infinity,),color: Theme.of(context).primaryColor),
+          SizedBox(
+            height: kDefaultMarginHeight.h,
+          ),
+          _summaryList(context, "Total Qty", "29040 MMK"),
+          SizedBox(height: kLargeMarginHeight.h,),
+          _summaryList(context, "Total Qty", "29040 MMK"),
+          SizedBox(height: kLargeMarginHeight.h,),
+          _summaryList(context, "Total Qty", "29040 MMK"),
+          SizedBox(height: kLargeMarginHeight.h,),
+          _summaryList(context, "Total Qty", "29040 MMK",color: Theme.of(context).primaryColor),
+        ],
+      ),
+
     );
   }
+}
+Widget _summaryList(context,title,price,{Color? color}) {
+
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Flexible(child: TextView(title: title, fontSize: kMediumFont14.sp,maxLine: 3,fontWeight: FontWeight.bold,)),
+      SizedBox(width: kDefaultMarginWidth.w,),
+      TextView(title: price, fontSize: kMediumFont14.sp,maxLine: 3,color: color?? Theme.of(context).hintColor,fontWeight: FontWeight.bold,)
+    ],
+  );
 }
